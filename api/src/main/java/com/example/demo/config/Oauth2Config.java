18d7cf0beb239c9ebd9bfb7efd262d7441d2f48e
ysfn <ystfnsh@gmail.com>
2022-01-01 07:39:32 +0200

    refactor aouth config

--- api/src/main/java/com/example/demo/config/Oauth2Config.java
+++ api/src/main/java/com/example/demo/config/Oauth2Config.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -9,31 +8,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.stereotype.Component;
 
 @Configuration
 public class Oauth2Config extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private Oauth2AuthenticationSuccessfulHandler oAuth2AuthenticationSuccessHandler;
+    @Value("${app.login.redirect-url}")
+    private String redirectUrl;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().authorizeRequests(a -> a
-                        .anyRequest().authenticated()
-                )
+        http.cors().and().authorizeRequests(a -> a.anyRequest().authenticated())
                 .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .logout(l -> l.logoutSuccessUrl("/").permitAll())
                 .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .oauth2Login()
-                .successHandler(oAuth2AuthenticationSuccessHandler);
-    }
-}
-
-@Component
-class Oauth2AuthenticationSuccessfulHandler extends SimpleUrlAuthenticationSuccessHandler {
-
-    public Oauth2AuthenticationSuccessfulHandler(@Value("${app.login.redirect-url}") String redirectUrl) {
-        super(redirectUrl);
+                .successHandler(new SimpleUrlAuthenticationSuccessHandler(redirectUrl));
     }
 }
